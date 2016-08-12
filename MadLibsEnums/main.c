5c446dc19d434f1b220957157b219701b2caf721
Princess Sampson <ps@kingonomics.com>
2016-08-12 11:05:24 -0400

    Initial Commit

---
+++ MadLibsEnums/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MadLibsEnums
+//
+//  Created by Princess Sampson on 8/12/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
